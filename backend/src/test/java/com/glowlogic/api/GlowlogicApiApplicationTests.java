7c0aae774cef0caae7c059e3064d09b8d0555d02
AgusBalles <ag.ballesteros@duocuc.cl>
2026-07-14 15:39:25 -0400

    build: configurar el backend y la base de datos iniciales

---
+++ backend/src/test/java/com/glowlogic/api/GlowlogicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glowlogic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowlogicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
